004842f1903c4452ca504fa702f614387de124e0
David O'Dey <dodey@icloud.com>
2019-11-22 11:00:02 -0700

    Updates

---
+++ test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Flayvor.css'
+
+const YourComponent = (props) => {
+  return (
+    <flayvor-top-app-bar
+      dense="true"
+      title="Flayvor Top App Bar"
+      menuicon="menu"
+    >
+    </flayvor-top-app-bar>
+  );
+};
+export default YourComponent
+
