ba24fe00d6c405136c155b6e4cf24cea39aa6dae
David O'Dey <dodey@icloud.com>
2019-11-26 20:48:35 -0700

    Updates

---
+++ precache-manifest.4a47dbf47dfafa9fc64e4313c14a011b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfefeb770f4000ce3656154a4fe9fc7d",
+    "url": "/wc-theme-selector/index.html"
+  },
+  {
+    "revision": "ad88c057a9b9f5944f4d",
+    "url": "/wc-theme-selector/static/css/main.4ca30385.chunk.css"
+  },
+  {
+    "revision": "620b10757547fb63ad96",
+    "url": "/wc-theme-selector/static/js/2.4074770e.chunk.js"
+  },
+  {
+    "revision": "ad88c057a9b9f5944f4d",
+    "url": "/wc-theme-selector/static/js/main.ec238742.chunk.js"
+  },
+  {
+    "revision": "e6b22d8d3b2b137ba99c",
+    "url": "/wc-theme-selector/static/js/runtime-main.73a8bc71.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wc-theme-selector/precache-manifest.aab3ac5c942b83d91856b1d3913c9da1.js"
+  "/wc-theme-selector/precache-manifest.4a47dbf47dfafa9fc64e4313c14a011b.js"
 );
 
 self.addEventListener('message', (event) => {
